aef8cf70373f17a04d82b4f5a3275e5f8828ecfb
jhkim <as04263@gmail.com>
2024-08-23 18:53:50 +0900

    model, layer 의 기본 구현, add 와 그 외 필요 함수

--- dev/layers/layer.py
+++ dev/layers/layer.py
@@ -1,3 +1,27 @@
 class Layer():
-    def __init__(self, name):
+    def __init__(self, name, input_shape =None):
         self.name = name
+        self.input_shape = input_shape
+        self.weights = None
+        self.trainable = True
+
+    # 가중치 초기화
+    def build(self):
+        pass
+    
+    # 연산
+    def call(self):
+        pass
+
+    def get_config(self):
+        # 레이어의 구성 정보 반환
+        return {
+            "name": self.name,
+            "input_shape": self.input_shape,
+            "trainable": self.trainable
+        }
+
+    @classmethod
+    def from_config(cls, config):
+        # 구성 정보로부터 레이어 생성
+        return cls(**config)
--- dev/models/model.py
+++ dev/models/model.py
@@ -1,5 +1,5 @@
-from layers.layer import Layer
-from decorators import check_layer_type
+from dev.layers.layer import Layer
+from dev.decorators import check_layer_type
 
 
 class Model():
@@ -10,14 +10,30 @@ class Model():
     def __init__(self, *args, **kwargs):
         # 모델 초기화 시 레이어를 담을 리스트를 준비합니다.
         self._layers = []
-        pass
+    
 
-    @check_layer_type
-    def add_layer(self, layer):
-        # 모델에 레이어를 추가합니다.
-        if not isinstance(layer, Layer):
-            raise TypeError("Only instances of Layer can be added")
-        self._layers.append(layer)
+    def add(self, layer):
+        if len(self.layers) == 0 and layer.input_shape is None:
+            raise ValueError("Input shape must be defined for the first layer.")
+        if len(self.layers) > 0 and layer.input_shape is None:
+            layer.input_shape = self.layers[-1].input_shape
+        
+        layer.build()
+        self.layers.append(layer)
+
+    def save(self):
+        # 모델 저장 (구성 정보와 가중치)
+        model_config = [layer.get_config() for layer in self.layers]
+        return model_config
+
+    @classmethod
+    def load(cls, model_config):
+        # 저장된 구성 정보로부터 모델 복원
+        model = cls()
+        for config in model_config:
+            layer = Layer.from_config(config)
+            model.add(layer)
+        return model
     
     @property
     def layers(self):
