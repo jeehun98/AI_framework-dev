7269cf81e273b0384e8dd21d1e23a394ec6430d4
jhkim <as04263@gmail.com>
2024-08-28 18:52:48 +0900

    build 의 구현, 가중치의 생성, 각 layer 인스턴스 내 저장

--- dev/layers/core/dense.py
+++ dev/layers/core/dense.py
@@ -6,16 +6,18 @@ class Dense(Layer):
     def __init__(self, units, activation=None, name=None, **kwargs):
         super().__init__(name)
         self.units = units
+        self.output_shape = (units,)
         self.activation = activations.get(activation)
         self.weights = None
         self.bias = None
 
     def build(self, input_shape):
         # 입력 차원에 따라 가중치와 편향을 초기화합니다.
+        # 입력 차원은 이전 layer 의 출력 차원이 될 것
         input_dim = input_shape[-1]
         self.weights = np.random.randn(input_dim, self.units)
         self.bias = np.zeros((self.units,))
-        super().build(input_shape)
+        #super().build(input_shape)
 
     def call(self, inputs):
         # 가중치와 편향을 적용하고 활성화 함수를 통해 출력합니다.
@@ -30,7 +32,7 @@ class Dense(Layer):
     def get_config(self):
         base_config = super().get_config()
         config = ({
-            'input_shape': self.input_shape,
+            'units': self.units,
             'activation': self.activation,
         })
         return {**base_config, **config}
