dd5bc8b036d00023edd6935b7a87887c4a673c39
jhkim <as04263@gmail.com>
2024-09-27 09:25:08 +0900

    learning_rate 적용 완료~

--- dev/backend/optimizers/binding_optimizers.cpp
+++ dev/backend/optimizers/binding_optimizers.cpp
@@ -6,10 +6,10 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(optimizers, m) {
-    py::class_<SGD>(m, "SGD")
-        .def(py::init<double>(), py::arg("learning_rate"))
-        .def("update", &SGD::update, py::arg("node"))  // 바인딩에 문제가 없는지 확인
-        .def("update_all_weights", &SGD::update_all_weights, py::arg("root"));  // 올바른 메서드가 바인딩되어 있는지 확인
+     py::class_<SGD>(m, "SGD")
+        .def(py::init<double>(), py::arg("learning_rate"))  // 초기화 메서드와 파라미터를 지정
+        .def("update", &SGD::update, py::arg("node"), py::arg("learning_rate"))  // update 메서드 바인딩
+        .def("update_all_weights", &SGD::update_all_weights, py::arg("root"), py::arg("learning_rate"));  // update_all_weights 바인딩
 
     py::class_<Adam>(m, "Adam")
         .def(py::init<double, double, double, double>(),
