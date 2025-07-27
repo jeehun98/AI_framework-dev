12d8e95a24cf432bc30c2494b8794023d06342b2
jh.kim <as04263@naver.com>
2025-07-27 18:23:31 +0900

    불필요 파일 삭제

--- dev/activations/__init__.py
+++
@@ -1,3 +0,0 @@
-# dev/activations/__init__.py
-
-from .activations_mapping import relu_graph, sigmoid_graph, tanh_graph
--- dev/decorators/__init__.py
+++
@@ -1,2 +0,0 @@
-from .validation_decorators import data_validation
-from .validation_decorators import check_layer_type
